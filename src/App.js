33e56f53e401f9a833e14cb2038f144ee37687e2
mohmmed-mahsoub <mohamedmahsoub181@gmail.com>
2020-11-18 13:25:24 +0200

    fetch api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Nav from "./components/nav";
 import Search from "./components/Search";
 import Tracks from "./components/Tracks";
+import ContextProvider from "./context";
 function App() {
   return (
     <div className="App">
       <Nav />
       <div className="container">
-        <Search />
-        <Tracks />
+        <ContextProvider>
+          <Search />
+          <Tracks />
+        </ContextProvider>
       </div>
     </div>
   );
--- src/components/Tracks.jsx
+++ src/components/Tracks.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import Track from "./Track";
+import { Context } from "../context";
 class Tracks extends Component {
   state = {};
   render() {
     return (
-      <React.Fragment>
-        <h3 className="text-center mb-4">top 10 tracks</h3>
-        <div className="row">
-          <Track />
-        </div>
-      </React.Fragment>
+      <Context.Consumer>
+        {(context) => {
+          console.log(context);
+          return (
+            <React.Fragment>
+              <h3 className="text-center mb-4">top 10 tracks</h3>
+              <div className="row">
+                <Track />
+              </div>
+            </React.Fragment>
+          );
+        }}
+      </Context.Consumer>
     );
   }
 }
