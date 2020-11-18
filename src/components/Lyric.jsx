88ed92df149ced3f740370d9845185f38e189a80
mohmmed-mahsoub <mohamedmahsoub181@gmail.com>
2020-11-18 17:13:03 +0200

    fetch data for this components that's why we don't put it in context file

--- src/components/Lyric.jsx
+++ src/components/Lyric.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import axiox from "axios";
+
 class Lyric extends Component {
-  state = {};
+  state = {
+    lyricData: {},
+    trackData: {},
+  };
+  componentDidMount = async () => {
+    const resLyrics = await axiox.get(
+      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    this.setState({ lyricData: resLyrics.data.message.body.lyrics });
+    const resTrack = await axiox.get(
+      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    this.setState({ trackData: resTrack.data.message.body.track });
+  };
   render() {
-    console.log(this.props.match.params.id);
+    console.log(this.state);
     return <div>lyric id {this.props.match.params.id}</div>;
   }
 }
