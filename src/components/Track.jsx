6f75ca25666b9e1b811708dc6f55444c17581c2f
mohmmed-mahsoub <mohamedmahsoub181@gmail.com>
2020-11-18 14:08:47 +0200

    showing tracks data dynamicly by context api

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -2,21 +2,23 @@ import React, { Component } from "react";
 class Track extends Component {
   state = {};
   render() {
+    const { trackData } = this.props;
+    console.log(trackData);
     return (
       <div className="col-md-6">
         <div className="card mb-4 shadow-sm">
           <div className="card-body">
-            <h5>track artist name</h5>
+            <h5>{trackData.artist_name}</h5>
             <p className="card-text">
               <strong>
                 <i className="fas fa-play" /> Track
               </strong>
-              track artist name
+              {trackData.track_name}
               <br />
               <strong>
                 <i className="fas fa-compact-disc" /> Album
               </strong>
-              track artist name
+              {trackData.album_name}
             </p>
             <button className="btn btn-dark btn-block">
               <i className="fas fa-chevron-right" /> View Lyrics
